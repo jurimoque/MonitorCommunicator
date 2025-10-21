2dbefe5777a7422bb1094d6fe99166afb058e804
jurimoque <132654730+jurimoque@users.noreply.github.com>
2025-10-21 18:13:34 +0200

    Fix(android): Implement splash screen programmatically to prevent crash

--- android/app/src/main/java/com/example/monitorcommunicator/MainActivity.java
+++ android/app/src/main/java/com/example/monitorcommunicator/MainActivity.java
@@ -1,5 +1,14 @@
 package com.example.monitorcommunicator;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    // Set the custom splash screen layout
+    setContentView(R.layout.launch_screen);
+    
+    super.onCreate(savedInstanceState);
+  }
+}
