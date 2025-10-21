5aedfa689f966b6d42c1fa25e812051fa2bbc31d
jurimoque <132654730+jurimoque@users.noreply.github.com>
2025-10-21 18:44:17 +0200

    Revert(android): Remove custom splash screen causing startup issues

--- android/app/src/main/java/com/example/monitorcommunicator/MainActivity.java
+++ android/app/src/main/java/com/example/monitorcommunicator/MainActivity.java
@@ -1,14 +1,5 @@
 package com.example.monitorcommunicator;
 
-import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    // Set the custom splash screen layout
-    setContentView(R.layout.launch_screen);
-    
-    super.onCreate(savedInstanceState);
-  }
-}
+public class MainActivity extends BridgeActivity {}
